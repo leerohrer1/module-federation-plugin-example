4c35f075e6eb9f72cb7639d7af7b8c3538e3fdb7
leerohrer1 <loginlee3@gmail.com>
2022-07-13 14:16:52 -0500

    Step 6 complete: Explicitly Configure Shared Dependencies

--- projects/shell/webpack.config.js
+++ projects/shell/webpack.config.js
@@ -1,13 +1,36 @@
-const { shareAll, withModuleFederationPlugin } = require('@angular-architects/module-federation/webpack');
+const {
+  share,
+  withModuleFederationPlugin,
+} = require("@angular-architects/module-federation/webpack");
 
 module.exports = withModuleFederationPlugin({
-
   remotes: {
-    // "mfe1": "http://localhost:4201/remoteEntry.js",    
+    // "mfe1": "http://localhost:4201/remoteEntry.js",
   },
 
-  shared: {
-    ...shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto' }),
-  },
+  shared: share({
+    "@angular/core": {
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: "auto",
+    },
+    "@angular/common": {
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: "auto",
+    },
+    "@angular/common/http": {
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: "auto",
+    },
+    "@angular/router": {
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: "auto",
+    },
+  }),
+
+  sharedMappings: ['auth-lib'],
 
 });
